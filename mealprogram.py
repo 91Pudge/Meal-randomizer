e127173d16942096b583c1d09af638f723fec2d3
91Pudge <73273137+91Pudge@users.noreply.github.com>
2021-11-22 09:49:14 +0900

    Add files via upload

---
+++ mealprogram.py
@@ -0,0 +1,38 @@
+
+import random
+
+
+meals = (
+    'ratatouie',
+    'spaghetti bolognese',
+    'lasnage',
+    'sausage and mash',
+    'teriyaki chicken',
+    'iyashino um may bo shi',
+    'hallumi wrap',
+    'salmon and spinach soup',
+    'cream soup',
+    'poatoe soup',
+    'tomato and lentil',
+    'dahl',
+    'steak & homemade chips',
+    'chichen & tamago don',
+    'ginger pork',
+    'chicken mustard',
+    'sushi-don',
+    'beef & rice',
+    'cottage pie',
+    #'',
+    #'',
+    #'',
+
+
+    )
+
+
+
+
+
+print("the first choice of what food we are going to eat is-- " + random.choice(meals))
+print("the 2nd choice of what food we are going to eat is--" + random.choice(meals))
+print("the 3rd choice of what food we are going to eat is--" + random.choice(meals))
